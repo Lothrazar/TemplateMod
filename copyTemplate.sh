64720d1e600816b7f675f27a5a9ddbf6626402b5
sam <samson.bassett@gmail.com>
2019-08-17 08:58:23 -0700

    new script to copy mod template to another folder

--- copyTemplate.sh
+++ copyTemplate.sh
@@ -2,24 +2,26 @@
 
 echo "Useage: ./_.sh <destination>"
 
+if [ -z "$1" ]
+  then
+    echo "No folder name supplied as first argument"
+	exit 
+fi
+
 dest=$1
+
+echo "copying to folder ../${dest}"
 
 declare -a arr=("dist.bat" "README.md" "gradle" "src" "build.gradle" ".gitignore" 
 "gradle.properties" "gradlew" "gradlew.bat" "refresh.bat")
 
-## now loop through the above array
+# copy each file/folder
+
 for file in "${arr[@]}"
 do
 	echo "$file"
 	cp -r "${file}" "../${dest}/${file}"
 done
-
-
-echo "copying to folder ../${dest}"
-
-# create the files
-
-
 
 
 
